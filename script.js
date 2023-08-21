d37eafc408e6fd394fdb6fc2e085ee35b0183f82
Graciane navarro <139176461+gracinha33@users.noreply.github.com>
2023-08-20 23:04:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var titulo = document.getElementById("titulo");
+    titulo.innerHTML = "Pesquisa de Satisfação";
+    titulo.style.color = "blue"; 
+
+    alert("É necessário responder a pesquisa!");
+  });
+  
+
+function mostrarAlerta() {
+  alert("Obrigado por responder à pesquisa!");
+}
